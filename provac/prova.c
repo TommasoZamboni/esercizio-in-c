58c1e1357145c4db46968f65d8aa2b5dd4df8de8
Tommaso Zamboni <tommaso.zamboni@iisviolamarchesini.edu.it>
2022-03-07 17:14:45 +0100

    esercizio in c

---
+++ provac/prova.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Stampa(void);
+int main()
+{
+Stampa();
+return 0;
+}
+
+void Stampa(void)
+{
+printf("*************************");
+printf("**                                    **");
+printf("*************************");
+}
---
+++ provac/prova2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Stampa(void);
+int main()
+{
+Stampa();
+return 0;
+}
+
+void Stampa(void)
+{
+printf("*************************");
+printf ("**            TPSIT             **");
+printf("*************************");
+}
+

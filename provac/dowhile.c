1355db4c3106ff0f82f0d5c33117cc4d910dc95b
Tommaso Zamboni <tommaso.zamboni@iisviolamarchesini.edu.it>
2022-03-14 20:57:22 +0100

    esercizio cicli

---
+++ provac/dowhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int contatore=0;
+do
+{
+contatore=contatore+1;
+printf("TPSIT");
+}while(contatore<=10);
+
+return 0;
+}
---
+++ provac/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Stampa(void);
+int main()
+{
+int contatore=0;
+while(contatore<=10)
+{
+contatore=contatore+1;
+printf("TPSIT");
+}
+return 0;
+}

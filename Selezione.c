e6a8be40310ee1344f619082248f1444383d9bb3
Tommaso Zamboni <tommaso.zamboni@iisviolamarchesini.edu.it>
2022-03-21 18:05:06 +0100

    selezione Switch

--- Selezione.c
+++ Selezione.c
@@ -3,21 +3,15 @@ int main(void){
 int giorni;
 printf("inserisci un numero del giorno: ");
 scanf("%d",&giorni);
-if(giorni==1)
-printf("il giorno è lunedì");
-if(giorni==2)
-printf("il giorno è martedì");
-if(giorni==3);
-printf("il giorno è mercoledì");
-if(giorni==4)
-printf("il giorno è giovedì");
-if(giorni==5)
-printf("il giorno è venerdì");
-if(giorni==6)
-printf("il giorno è sabato");
-if(giorni==7)
-printf("il giorno è domenica");
-if(giorni>7)
-printf("giorno non valido");
+switch(giorni){
+case 1:printf("il giorno è lunedì");break;
+case 2:printf("il giorno è martedì");break;
+case 3:printf("il giorno è mercoledì");break;
+case 4:printf("il giorno è giovedì");break;
+case 5:printf("il giorno è venerdì");break;
+case 6:printf("il giorno è sabato");break;
+case 7:printf("il giorno è domenica");break;
+default:printf("giorno non valido");break;
+}
 return 0;
 }
